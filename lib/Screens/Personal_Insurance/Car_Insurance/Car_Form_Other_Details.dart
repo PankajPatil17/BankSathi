8860ea759c69b122dfca38099d975e5232b512f1
Pankaj <patilpankaj1772@gmail.com>
2023-01-13 14:21:33 +0530

    changes

---
+++ lib/Screens/Personal_Insurance/Car_Insurance/Car_Form_Other_Details.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:bank_sathi/Controller/CarInsurance_Controller.dart';
+import 'package:bank_sathi/Generic/Common/CommonBottomBar.dart';
+import 'package:bank_sathi/Generic/Common/CommonTextNunito.dart';
+import 'package:bank_sathi/Generic/Common/Common_Text.dart';
+import 'package:bank_sathi/Generic/Constant/colors.dart';
+import 'package:bank_sathi/Generic/Constant/variables.dart';
+import 'package:bank_sathi/Screens/Personal_Insurance/Car_Insurance/Car_Insurance_Home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:sizer/sizer.dart';
+import 'package:get/get.dart';
+
+class CarFormOtherDetails extends StatefulWidget {
+  CarFormOtherDetails({super.key});
+
+  @override
+  State<CarFormOtherDetails> createState() => _CarFormOtherDetailsState();
+}
+
+class _CarFormOtherDetailsState extends State<CarFormOtherDetails> {
+  var carinsurancecontroller = Get.put(CarInsuranceController());
+  TextEditingController search = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: PWhite,
+      appBar: PreferredSize(
+          child: SafeArea(
+              child: Container(
+            height: 7.h,
+            width: 100.h,
+            padding: EdgeInsets.only(right: 1.2.h),
+            decoration: BoxDecoration(color: Colors.white, boxShadow: [
+              BoxShadow(
+                  color: Colors.black12, blurRadius: 2, offset: Offset(0, 4))
+            ]),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    icon: Icon(
+                      Icons.arrow_back_ios_rounded,
+                      color: Colors.black,
+                    )),
+                SvgPicture.asset('assets/Images/BankSathi.svg'),
+              ],
+            ),
+          )),
+          preferredSize: Size(100.h, 20.h)),
+    );
+  }
+}
